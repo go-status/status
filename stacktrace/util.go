79754b2aef359babdb289e38c03d9119eb2e31d2
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2022-11-11 13:43:12 +0900

    .

--- stacktrace/util.go
+++ stacktrace/util.go
@@ -15,17 +15,23 @@ import (
 // and a full path (e.g., "path/to/pkg.Func@/path/to/format.go:123").
 // Otherwise, a stack frame contains short names (e.g., "Func@format.go:123").
 func ToString(s *stpb.StackTrace, verbose bool) string {
+	// If no frames exist, returns "No stack trace" for better readability.
 	if len(s.GetFrames()) == 0 {
 		return "No stack trace"
 	}
 
+	// Prepare a string builder because concatenating strings is inefficient.
 	var w strings.Builder
 
+	// Append a stack trace heading.
 	w.WriteString("Stack trace:")
 
+	// Append all frames in a human-readable format.
 	for _, frame := range s.GetFrames() {
+		// Make every stack frame have an indent of two spaces.
 		w.WriteString("\n  ")
 
+		// Append a function name.
 		if verbose {
 			w.WriteString(frame.GetFunction())
 		} else {
@@ -33,17 +39,21 @@ func ToString(s *stpb.StackTrace, verbose bool) string {
 			w.WriteString(frame.GetFunction()[p+1:])
 		}
 
+		// Append a separator between a function name and a file name.
 		w.WriteString("@")
 
+		// Append a file name (verbose=false) or a full path (verbose=false).
 		if verbose {
 			w.WriteString(frame.GetFile())
 		} else {
 			w.WriteString(path.Base(frame.GetFile()))
 		}
 
+		// Append a line number.
 		w.WriteString(":")
 		w.WriteString(strconv.Itoa(int(frame.GetLine())))
 
+		// Append a program counter.
 		if verbose && frame.GetProgramCounter() != 0 {
 			w.WriteString("(")
 
@@ -56,5 +66,6 @@ func ToString(s *stpb.StackTrace, verbose bool) string {
 		}
 	}
 
+	// Build a string and returns it.
 	return w.String()
 }
