fb8f53912eca4e08407460d722caf469c0cf45e2
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2022-12-04 02:25:29 +0900

    .

--- status.go
+++ status.go
@@ -1,18 +1,20 @@
-// Package status enables all errors to have stack traces while pkg/errors
-// cannot enforce it.
+// Package status enables all errors to have stack traces.  This is useful
+// because the pkg/errors package does not enforce the inclusion of stack
+// traces.
 package status
 
-// Status is an error interface having an error code, an error message and
-// a stack trace.  It can also have an error chain.  As well as the error
-// interface, nil represents non-error (i.e., OK in the gRPC context).
-// NOTE: Status does not implement methods other than Error() because Status
-// can be nil and calling a method of nil panics.
+// Status is an error interface that contains an error code, error message,
+// and stack trace.   It can also have an error chain.  In addition, it
+// implements the built-in error interface so that it can be down-casted to
+// an error type.  A nil value of Status represents a non-error (i.e. OK in the
+// gRPC context).
+// NOTE: Status does not implement methods other than Error() because calling
+// a method of a nil Status value will cause a panic.
 type Status interface {
-	// Implements the builtin `error` interface so that Status can be
-	// down-casted to error.
+	// Implements the built-in error interface to allow casting to error.
 	error
 
-	// statusImpl returns the inner statusImpl object.  Besides, it forces
-	// any Status to embed statusImpl.
+	// statusImpl returns the inner statusImpl object and forces any Status to
+	// embed statusImpl.
 	statusImpl() *statusImpl
 }
