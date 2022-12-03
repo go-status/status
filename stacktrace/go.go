d5f9de8c484c59b8ae6da618f3e87a9d8ca8e27d
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2022-12-04 01:08:17 +0900

    .

--- stacktrace/go.go
+++ stacktrace/go.go
@@ -6,25 +6,25 @@ import (
 	"strings"
 )
 
-// Go spawns a go routine calling f with contextual stack tracing.  While a
-// regular go routine discards stack frames of its caller, this saves the
-// caller's stack frames and provides them to a spawned go-routine via Context.
+// Go spawns a go routine calling f with contextual stack tracing.  Unlike a
+// regular go routine, this saves the caller's stack frames and provides them
+// to a spawned go-routine via Context.
 func Go(ctx context.Context, f func(context.Context)) {
 	// Record the current stack trace.
 	st := goExit(ctx)
 
-	// Call the give function inside a go-routine.
+	// Call the given function inside a go-routine.
 	go goEnter(func() { f(contextWith(ctx, st)) })
 }
 
-// goEnterFuncName is a fully-qualified function name of goEnter.
+// goEnterFuncName is the fully-qualified function name of goEnter.
 //
 //nolint:gochecknoglobals
 var goEnterFuncName = func() string {
 	var name string
 
 	// Acquire a stack frame of goEnter and store the fully-qualified function
-	// name of goEnter to name.
+	// name of goEnter in the name variable.
 	goEnter(func() {
 		pc, _, _, _ := runtime.Caller(1)
 		details := runtime.FuncForPC(pc)
